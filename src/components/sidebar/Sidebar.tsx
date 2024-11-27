4984627e7e9e331fdcb081d1211fadaa2da2b58b
Miguel CAballero <miguelhitogarciaalturo@gmail.com>
2024-11-27 14:03:14 -0500

    stiky sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -34,38 +34,42 @@ export const Sidebar = () => {
     return (
         <div id="menu"
             style={{ width: '400px' }}
-            className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
-            <div id="logo" className="my-4 px-6">
-                <h1 className="text-lg md:text-2xl font-bold text-white flex items-center ">
-                    <IoLogoReact className="mr-2" />
-                    <span>Dash</span>
-                    <span className="text-blue-500">8</span>.
-                </h1>
-                <p className="text-slate-500 text-sm">Manage your actions and activities</p>
-            </div>
-            <div id="profile" className="px-6 py-10">
-                <p className="text-slate-500">Welcome back,</p>
-                <a href="#" className="inline-flex space-x-2 items-center">
-                    <span>
-                        <Image className="rounded-full w-8 h-8"
-                            src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
-                            alt="User avatar"
-                            width={50}
-                            height={50}
-                        />
-                    </span>
-                    <span className="text-sm md:text-base font-bold">
-                        Miguel Caballero
-                    </span>
-                </a>
-            </div>
-            <div id="nav" className="w-full px-6">
+            className="table bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
+            <div className="sticky top-0">
+                <div id="logo" className="my-4 px-6">
+                    <h1 className="text-lg md:text-2xl font-bold text-white flex items-center ">
+                        <IoLogoReact className="mr-2" />
+                        <span>Dash</span>
+                        <span className="text-blue-500">8</span>.
+                    </h1>
+                    <p className="text-slate-500 text-sm">Manage your actions and activities</p>
+                </div>
+                <div id="profile" className="px-6 py-10">
+                    <p className="text-slate-500">Welcome back,</p>
+                    <a href="#" className="inline-flex space-x-2 items-center">
+                        <span>
+                            <Image className="rounded-full w-8 h-8"
+                                src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
+                                alt="User avatar"
+                                width={50}
+                                height={50}
+                            />
+                        </span>
+                        <span className="text-sm md:text-base font-bold">
+                            Miguel Caballero
+                        </span>
+                    </a>
+                </div>
+                <div id="nav" className="w-full px-6">
 
-                {menuItems.map(item => (
-                    <SibarMenuItem key={item.path} {...item} />
-                ))}
+                    {menuItems.map(item => (
+                        <SibarMenuItem key={item.path} {...item} />
+                    ))}
+
+                </div>
 
             </div>
+
         </div>
     )
 }
